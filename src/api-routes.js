0f32e8b940b2dd95c88479f313a12701ac516c68
Ayo Onamusi <onamusia@gmail.com>
2023-05-18 01:30:23 +0100

    lab 15a - v0.3.0

--- src/api-routes.js
+++ src/api-routes.js
@@ -1,4 +1,6 @@
 import { userApi } from "./api/users-api.js";
+import { donationsApi } from "./api/donations-api.js";
+import { candidatesApi } from "./api/candidates-api.js";
 
 export const apiRoutes = [
   { method: "GET", path: "/api/users", config: userApi.find },
@@ -6,4 +8,15 @@ export const apiRoutes = [
   { method: "DELETE", path: "/api/users", config: userApi.deleteAll },
   { method: "GET", path: "/api/users/{id}", config: userApi.findOne },
   { method: "POST", path: "/api/users/authenticate", config: userApi.authenticate },
+
+  { method: "GET", path: "/api/donations", config: donationsApi.findAll },
+  { method: "GET", path: "/api/candidates/{id}/donations", config: donationsApi.findByCandidate },
+  { method: "POST", path: "/api/candidates/{id}/donations", config: donationsApi.makeDonation },
+  { method: "DELETE", path: "/api/donations", config: donationsApi.deleteAll },
+
+  { method: "GET", path: "/api/candidates", config: candidatesApi.find },
+  { method: "GET", path: "/api/candidates/{id}", config: candidatesApi.findOne },
+  { method: "POST", path: "/api/candidates", config: candidatesApi.create },
+  { method: "DELETE", path: "/api/candidates/{id}", config: candidatesApi.deleteOne },
+  { method: "DELETE", path: "/api/candidates", config: candidatesApi.deleteAll },
 ];
